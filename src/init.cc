8dbd496da5647be1d8c620fdd2fd376076ef3110
William Lahti <wilahti@gmail.com>
2020-09-21 18:21:09 -0700

    expose all functions added so far

--- src/init.cc
+++ src/init.cc
@@ -1,15 +1,19 @@
 #include <libyuv.h>
-#include "version.h"
-#include "convert.h"
 #include "convert_argb.h"
+#include "convert.h"
+#include "cpu_id.h"
+#include "init.h"
+#include "rotate_argb.h"
+#include "rotate.h"
+#include "scale_argb.h"
+#include "scale.h"
+#include "version.h"
 
 void Init(Napi::Env env, Napi::Object exports) {
     
-    // version.cc
-    exports.Set("Version", Napi::Function::New(env, Version));
-
     // convert.cc
     exports.Set("I444ToI420", Napi::Function::New(env, I444ToI420));
+    exports.Set("I444ToNV12", Napi::Function::New(env, I444ToNV12));
     exports.Set("I444ToNV21", Napi::Function::New(env, I444ToNV21));
     exports.Set("I422ToI420", Napi::Function::New(env, I422ToI420));
     exports.Set("I422ToNV21", Napi::Function::New(env, I422ToNV21));
@@ -169,4 +173,52 @@ void Init(Napi::Env env, Napi::Object exports) {
     exports.Set("I400ToARGBMatrix", Napi::Function::New(env, I400ToARGBMatrix));
     exports.Set("ConvertToARGB", Napi::Function::New(env, ConvertToARGB));
 
+    // cpu_id
+
+    exports.Set("InitCpuFlags", Napi::Function::New(env, InitCpuFlags));
+    exports.Set("TestCpuFlag", Napi::Function::New(env, TestCpuFlag));
+    exports.Set("ArmCpuCaps", Napi::Function::New(env, ArmCpuCaps));
+    exports.Set("MipsCpuCaps", Napi::Function::New(env, MipsCpuCaps));
+    exports.Set("MaskCpuFlags", Napi::Function::New(env, MaskCpuFlags));
+    exports.Set("SetCpuFlags", Napi::Function::New(env, SetCpuFlags));
+    exports.Set("CpuId", Napi::Function::New(env, CpuId));
+
+    // rotate_argb.h
+
+    exports.Set("ARGBRotate", Napi::Function::New(env, ARGBRotate));
+
+    // rotate.h
+
+    exports.Set("I420Rotate", Napi::Function::New(env, I420Rotate));
+    exports.Set("I444Rotate", Napi::Function::New(env, I444Rotate));
+    exports.Set("NV12ToI420Rotate", Napi::Function::New(env, NV12ToI420Rotate));
+    exports.Set("RotatePlane", Napi::Function::New(env, RotatePlane));
+    exports.Set("RotatePlane90", Napi::Function::New(env, RotatePlane90));
+    exports.Set("RotatePlane180", Napi::Function::New(env, RotatePlane180));
+    exports.Set("RotatePlane270", Napi::Function::New(env, RotatePlane270));
+    exports.Set("RotateUV90", Napi::Function::New(env, RotateUV90));
+    exports.Set("RotateUV180", Napi::Function::New(env, RotateUV180));
+    exports.Set("RotateUV270", Napi::Function::New(env, RotateUV270));
+    exports.Set("TransposePlane", Napi::Function::New(env, TransposePlane));
+    exports.Set("TransposeUV", Napi::Function::New(env, TransposeUV));
+
+    // scale_argb.h
+
+    exports.Set("ARGBScale", Napi::Function::New(env, ARGBScale));
+    exports.Set("ARGBScaleClip", Napi::Function::New(env, ARGBScaleClip));
+    exports.Set("YUVToARGBScaleClip", Napi::Function::New(env, YUVToARGBScaleClip));
+
+    // scale.h
+
+    exports.Set("ScalePlane", Napi::Function::New(env, ScalePlane));
+    exports.Set("ScalePlane_16", Napi::Function::New(env, ScalePlane_16));
+    exports.Set("I420Scale", Napi::Function::New(env, I420Scale));
+    exports.Set("I420Scale_16", Napi::Function::New(env, I420Scale_16));
+    exports.Set("I444Scale", Napi::Function::New(env, I444Scale));
+    exports.Set("I444Scale_16", Napi::Function::New(env, I444Scale_16));
+    exports.Set("Scale", Napi::Function::New(env, Scale));
+
+    // version.h
+
+    exports.Set("Version", Napi::Function::New(env, Version));
 }
