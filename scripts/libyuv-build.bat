2bd6fdf66b959aeec447d9352d49442dde4b5a32
William Lahti <wilahti@gmail.com>
2020-09-23 01:27:14 -0700

    be less specific about profile (relying on -A x64 to do the work)

--- scripts/libyuv-build.bat
+++ scripts/libyuv-build.bat
@@ -6,7 +6,7 @@ git clone https://chromium.googlesource.com/libyuv/libyuv
 cd libyuv
 mkdir out
 cd out 
-cmake -G "Visual Studio 15 2017" -A x64 -DCMAKE_BUILD_TYPE="Release" ..
+cmake -A x64 -DCMAKE_BUILD_TYPE="Release" ..
 cmake --build . --config Release
 copy Release\yuv.lib ..\..
 copy Release\libyuv.dll ..\..
