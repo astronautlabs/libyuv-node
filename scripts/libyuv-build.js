7989fc0c859ff9484fdb154b74b1df3cbb37a74c
William Lahti <wilahti@gmail.com>
2020-09-22 22:21:40 -0700

    call script using bash to avoid requiring chmod +x

--- scripts/libyuv-build.js
+++ scripts/libyuv-build.js
@@ -1,10 +1,20 @@
-const { spawn } = require('child_process');
+const { spawn, } = require('child_process');
 const path = require('path');
-
+const fs = require('fs');
+const os = require('os');
 
 let script = 'scripts/libyuv-build.sh';
-if (process.platform === 'win32')
+let args = [];
+
+if (process.platform === 'win32') {
     script = script.replace(/\.sh$/, '.bat');
+}
 console.log(`> ${script}`);
 script = path.resolve(path.join(__dirname, '..', script));
-spawn(script, { stdio: 'inherit'});
+
+if (process.platform !== 'win32') {
+    args.unshift(script);
+    script = 'bash';
+}
+
+spawn(script, args, { stdio: 'inherit'});
