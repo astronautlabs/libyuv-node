54492f4454c79418e3e22eef70f5511c47f478ba
William Lahti <wilahti@gmail.com>
2020-09-23 16:54:48 -0700

    adjust import for macos

--- binding.js
+++ binding.js
@@ -1,5 +1,8 @@
+// on macOS node-gyp auto-removes lib prefix (not sure why or how to stop it)
+let moduleName = process.platform === 'darwin' ? 'yuv' : 'libyuv';
+
 try {
-  module.exports = require('./build/Debug/libyuv.node');
+  module.exports = require(`./build/Debug/${moduleName}.node`);
 } catch (error) {
-  module.exports = require('./build/Release/libyuv.node');
+  module.exports = require(`./build/Release/${moduleName}.node`);
 }
