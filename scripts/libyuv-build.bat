001ea204ceb3363d2f144fa854e729c63f750ffa
William Lahti <wilahti@gmail.com>
2020-09-23 17:39:12 -0700

    Fix windows builds everywhere except CircleCI

--- scripts/libyuv-build.bat
+++ scripts/libyuv-build.bat
@@ -1,9 +1,11 @@
+rem Handles issues with CircleCI declaring both PATH and Path
+rem But must be handled carefully, because set PATH= will clear 
+rem PATH entirely when there is not two set. This should result 
+rem in a working situation in most cases 
 
-echo "ENVIRONMENT:"
-set
-
+set ORIGINAL_PATH=%Path%
 set PATH=
-set Path=%Path%C:\\Program Files\\CMake\\bin;
+set Path=%ORIGINAL_PATH%C:\\Program Files\\CMake\\bin;
 
 mkdir external
 cd external
